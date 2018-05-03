d9d84bd8f450d8eb1f47978f0fe2fe626036ac3e
Aaron Taylor <ataylor@subgeniuskitty.com>
2018-05-03 06:18:54 -0700

    Added help/usage and argument processing to C template.

--- src/main.c
+++ src/main.c
@@ -4,8 +4,38 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define VER_MAJOR 1
+#define VER_MINOR 0
+
+void print_usage(char ** argv) {
+    printf( "PROJECT_NAME %d.%d (www.subgeniuskitty.com)\n"
+            "Usage: %s [option] ...\n"
+            "  -h    Help (prints this message)\n"
+            , VER_MAJOR, VER_MINOR, argv[0]
+    );
+}
 
 int main(int argc, char ** argv) {
+    /*
+     * Process command line arguments
+     */
+    int c;
+    while((c = getopt(argc,argv,"h")) != -1) {
+        switch(c) {
+            case 'h':
+                print_usage(argv);
+                exit(EXIT_SUCCESS);
+                break;
+            default:
+                break;
+        }
+    }
+
     printf("Hello, World!\n");
-    return 0;
+
+    exit(EXIT_SUCCESS);
 }
