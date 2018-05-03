3ea5a6a1ebd55da7f95b14c9c59ceda53f74abf5
Aaron Taylor <ataylor@subgeniuskitty.com>
2018-05-03 06:04:08 -0700

    Adding hello world in C and corresponding Makefile.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+/*
+ * © 2017 Aaron Taylor <ataylor at subgeniuskitty dot com>
+ * See LICENSE file for copyright and license details.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char ** argv) {
+    printf("Hello, World!\n");
+    return 0;
+}
